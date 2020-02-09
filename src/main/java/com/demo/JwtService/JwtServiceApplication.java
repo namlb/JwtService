e67fb5862e47611818f473c90c7b78402c38182b
nam le ba <namlb1606@gmail.com>
2020-02-09 21:02:28 +0700

    initial_add_all

---
+++ src/main/java/com/demo/JwtService/JwtServiceApplication.java
@@ -0,0 +1,19 @@
+package com.demo.JwtService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class JwtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtServiceApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
---
+++ src/main/java/com/demo/JwtService/repository/ApplicationUserRepository.java
@@ -0,0 +1,8 @@
+package com.demo.JwtService.repository;
+
+import com.demo.JwtService.entity.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
